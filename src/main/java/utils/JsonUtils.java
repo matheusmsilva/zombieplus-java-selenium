dc65cc62f8efabc98f4e0384cec03ab6b3212ac9
matheusmsilva <sp.matheus.msilva@gmail.com>
2024-06-20 13:50:22 -0300

    turning json file path relative

--- src/main/java/utils/JsonUtils.java
+++ src/main/java/utils/JsonUtils.java
@@ -7,12 +7,12 @@ import java.io.File;
 import java.io.IOException;
 
 public class JsonUtils {
-    private static final String JSON_FILE_PATH = "E:/automation/automacao-java/zombieplus/src/test/resources/fixtures/movies.json";
+    private static final String JSON_FILE_PATH = "src/test/resources/fixtures";
 
     public static MovieData readMovieData() {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            return objectMapper.readValue(new File(JSON_FILE_PATH), MovieData.class);
+            return objectMapper.readValue(new File(JSON_FILE_PATH + "/movies.json"), MovieData.class);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
